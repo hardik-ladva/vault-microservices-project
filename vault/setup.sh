cf54efb6db10ed5a7d780696498b72a204d3bdea
Hardik Ladva <hardik.ladva001@gmail.com>
2025-06-15 23:10:09 -0400

    Updated the Setup.sh

--- vault/setup.sh
+++ vault/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 export VAULT_ADDR='http://127.0.0.1:8200'
-vault login root
+vault login $token
 
+vault secrets disable secret/
 vault secrets enable -path=secret kv-v2
 
 vault kv put secret/service1 DB_USER=app1 DB_PASS=app1pass
 vault kv put secret/service2 API_KEY=abcdef123456
 
-vault policy write service1-policy policies/service1-policy.hcl
-vault policy write service2-policy policies/service2-policy.hcl
+vault policy write service1-policy vault/policies/service1-policy.hcl
+vault policy write service2-policy vault/policies/service2-policy.hcl
 
 vault auth enable approle
 
